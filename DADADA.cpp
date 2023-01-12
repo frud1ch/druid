a5b912044c787b78110115a686d45dc3ac96a852
bppk <root@MB-C-3-9-PC12>
2023-01-12 14:46:38 +0300

    1

--- DADADA.cpp
+++ DADADA.cpp
@@ -14,16 +14,24 @@ public:
     A get1(int){
         return health;
     }
-};
 
+int calculatel()
+{
+    A sum = 10;
+        health = sum;
+    return health;
+}
+};
 using namespace std;
 
 int main()
 {
     Druid<int, int> One;
     Druid<int, int> Two;
-    One.health = 100;
+ 
+    One.health;
      Two.power = 30;
+     One.calculatel( );
     std::cout << One.get1(4) << endl;
     std::cout << Two.get(5) << endl;
     return 0;
